51872afcb6f1b1929373c1c242e1d5ac7bbbe435
buxata <vidolarez@gmail.com>
2025-07-10 15:11:30 +0300

    initial commit before functions working

---
+++ main.py
@@ -0,0 +1,39 @@
+import os,sys
+from dotenv import load_dotenv
+from google import genai
+
+from google.genai import types
+
+
+
+def main ():
+
+    prompt = ""
+    if len(sys.argv)>1:
+        prompt = sys.argv[1]
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=prompt)]),
+        ]
+        print(prompt)
+        load_dotenv()
+        api_key = os.environ.get("GEMINI_API_KEY")
+        print(api_key)
+        client = genai.Client(api_key=api_key)
+
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001',
+            contents=messages
+        )
+        print(response.text)
+        if len(sys.argv)>2:
+            if sys.argv[2] == "--verbose":
+                print(f"User prompt: {prompt}")
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    else:
+        print("No prompt given")
+        sys.exit(1)
+        return
+
+main()
